8d0a9812a455a417a9349a9b37b68af0daacdeb3
Stif <stifwoo@gmail.com>
2017-12-05 02:45:54 +0200

    Add cross-platform app project

--- IFCurrenciesApi/Helper/BankIdConsts.cs
+++ IFCurrenciesApi/Helper/BankIdConsts.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace IFCurrenciesApi.Helper
+﻿namespace IFCurrenciesApi.Helper
 {
     public static class BankIdConsts
     {
--- IFCurrenciesApi/Managers/FinanceManager.cs
+++ IFCurrenciesApi/Managers/FinanceManager.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using IFCurrenciesApi.Helper;
 using IFCurrenciesApi.Models.FinanceUaResponse;
 using IFCurrenciesApi.Services;
